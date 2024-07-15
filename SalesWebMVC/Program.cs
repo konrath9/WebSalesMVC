c8738145192774fa8b46991a48d8d0108bc219c5
Taynan Konrath <taynankonrath@gmail.com>
2024-07-15 19:17:58 -0300

    Implementing simple search

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
